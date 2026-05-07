2dab76aa56213ae60ae8581bb2a64f223652739b
diomedesf-sys <diomedesf@gmail.com>
2026-05-07 19:20:10 -0400

    small fix 1

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,27 +1,21 @@
-import { BrowserWindow, app } from "electron";
-import path from "path";
-import { fileURLToPath } from "url";
+import { BrowserWindow as e, app as t } from "electron";
+import n from "path";
+import { fileURLToPath as r } from "url";
 //#region electron/main.js
-var __filename = fileURLToPath(import.meta.url);
-var __dirname = path.dirname(__filename);
-var mainWindow;
-function createWindow() {
-	mainWindow = new BrowserWindow({
+var i = r(import.meta.url), a = n.dirname(i), o;
+function s() {
+	o = new e({
 		width: 1200,
 		height: 800,
 		webPreferences: {
-			nodeIntegration: true,
-			contextIsolation: false
+			nodeIntegration: !0,
+			contextIsolation: !1
 		}
-	});
-	if (process.env.VITE_DEV_SERVER_URL) mainWindow.loadURL(process.env.VITE_DEV_SERVER_URL);
-	else mainWindow.loadFile(path.join(__dirname, "../dist/index.html"));
+	}), process.env.VITE_DEV_SERVER_URL ? o.loadURL(process.env.VITE_DEV_SERVER_URL) : o.loadFile(n.join(a, "../dist/index.html"));
 }
-app.whenReady().then(createWindow);
-app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") app.quit();
-});
-app.on("activate", () => {
-	if (BrowserWindow.getAllWindows().length === 0) createWindow();
+t.whenReady().then(s), t.on("window-all-closed", () => {
+	process.platform !== "darwin" && t.quit();
+}), t.on("activate", () => {
+	e.getAllWindows().length === 0 && s();
 });
 //#endregion
